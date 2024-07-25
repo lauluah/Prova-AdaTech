6f361e6b286892f7cd546d0cb9ac43ae0695fdf2
Laura Marques <159571325+lauluah@users.noreply.github.com>
2024-07-25 19:51:12 -0300

    Update TestFuncionario.java

--- Prova/Domain/Test/TestFuncionario.java
+++ Prova/Domain/Test/TestFuncionario.java
@@ -6,25 +6,26 @@ import java.util.Scanner;
 
 public class TestFuncionario {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         for (int i = 1; i <= 5; i++) {
-            System.out.print("Digite o salário bruto do funcionário " + i + " =" );
-            double salarioSemDesconto = scanner.nextDouble();
+            System.out.println("Digite o salario do funcionario " + i + ": ");
+            double salarioBruto = scanner.nextDouble();
+            double salarioLiquidoTotal = 0;
 
-            double descontoINSS = CalcularDescontos.descontoDeInss(salarioSemDesconto);
-            double salarioBase = salarioSemDesconto - descontoINSS;
-            double descontoImpRenda = CalcularDescontos.descontoImpRenda(salarioBase);
-            double salarioLiquido = salarioBase - descontoImpRenda;
+            double salarioDescontoINSS = Desconto.descontoINSS(salarioBruto);
+            double salarioDescontoIR = Desconto.descontoImpRenda(salarioBruto - salarioDescontoINSS);
+            double salarioLiquido = salarioBruto - salarioDescontoIR;
 
-            System.out.println("Salário bruto: R$ " + salarioSemDesconto);
-            System.out.println("Desconto INSS: R$ " + descontoINSS);
-            System.out.println("Desconto IR: R$ " + descontoImpRenda);
+            salarioLiquidoTotal += salarioLiquido;
+
+            System.out.println("Salário bruto: R$ " + salarioBruto);
+            System.out.println("Desconto INSS: R$ " + salarioDescontoINSS);
+            System.out.println("Desconto IR: R$ " + salarioDescontoIR);
             System.out.println("Salário líquido: R$ " + salarioLiquido);
 
+            if(i == 5) { System.out.println(" ## Salário liquido total: " + salarioLiquidoTotal); }
         }
-
-        scanner.close();
     }
 }
